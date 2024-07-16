cd1606c791f88f0d5d13204193ff44f57f29e8d7
삭재형 <64705995+tjswogud@users.noreply.github.com>
2024-07-16 15:21:11 +0900

    2024.07.16 JH
    BombCharacter Code 몇 줄 제거

--- Source/ListenServerProject/Characters/BombCharacter.cpp
+++ Source/ListenServerProject/Characters/BombCharacter.cpp
@@ -181,11 +181,6 @@ void ABombCharacter::MultiSpawnBomb_Implementation(ABomb* SpawnBomb)
 
 void ABombCharacter::OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	/*if (ABombCharacter* HitActor = Cast<ABombCharacter>(OtherActor))
-	{
-		OnAttackSuccess(this, HitActor);
-	}*/
-
 	if (OtherActor && OtherActor != this && bAttack)
 	{
 		if (ABombCharacter* HitActor = Cast<ABombCharacter>(OtherActor))
