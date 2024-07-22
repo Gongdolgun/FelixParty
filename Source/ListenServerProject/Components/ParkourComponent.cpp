68783b2e3e963867c661b10b574abca191e4e78e
InsuMoon2 <soo93689314@gmail.com>
2024-07-22 23:04:25 +0900

    24.07.22 IS
    Parkour 구조 변경
    - 파쿠르 애니메이션이 맘에 안들어서 변경
    - 기존에는 높은 벽만 올라가는 애니메이션만 있었는데 이제는 LineTrace의 충돌 개수에 따라서 파쿠르의 타입을 정하고 파쿠르 타입에 맞는 몽타주 재생
    
    클라이언트는 ParkourType이 제대로 인식이 안되는 것 같음. 고쳐야하고, MaxSpeed 리플리케이트 하다가 캐릭터가 멈춰버리는 버그 발생.

--- Source/ListenServerProject/Components/ParkourComponent.cpp
+++ Source/ListenServerProject/Components/ParkourComponent.cpp
@@ -63,7 +63,6 @@ void UParkourComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& Ou
 	DOREPLIFETIME(ThisClass, AddPlayerLocationForward);
 	DOREPLIFETIME(ThisClass, AddPlayerLocationZ_High);
 	DOREPLIFETIME(ThisClass, AddPlayerLocationZ_Low);
-	DOREPLIFETIME(ThisClass, AvailableFrontAngle);
 
 	DOREPLIFETIME(ThisClass, falling_ImpactPoint);
 }
@@ -135,7 +134,18 @@ void UParkourComponent::ParkourTrace(FVector& OutLocation1, FVector& OutLocation
 
 	else if (bDownHit && OwnerCharacter->CanJump())
 	{
-		ParkourType = EParkourType::Low;
+		// Left, Right, Center 중 하나라도 충돌이 안되었는지 확인
+		if (!HitResults[(int32)EParkourArrowType::Left].bBlockingHit &&
+			!HitResults[(int32)EParkourArrowType::Right].bBlockingHit &&
+			!HitResults[(int32)EParkourArrowType::Center].bBlockingHit)
+		{
+			ParkourType = EParkourType::Low;
+		}
+
+		else
+		{
+			return;
+		}
 	}
 
 	// 모든 방향이 충돌했을 때 또는 Down 방향만 충돌했을 때
