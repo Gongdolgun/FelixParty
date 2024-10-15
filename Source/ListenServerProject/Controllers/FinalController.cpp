6b1802ace13fc39d5ee31f628ec998bc9b03d513
InsuMoon2 <soo93689314@gmail.com>
2024-10-15 17:18:22 +0900

    asdf

---
+++ Source/ListenServerProject/Controllers/FinalController.cpp
@@ -0,0 +1,18 @@
+#include "Controllers/FinalController.h"
+
+AFinalController::AFinalController()
+{
+
+}
+
+void AFinalController::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void AFinalController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+}
---
+++ Source/ListenServerProject/GameState/FinalGameState.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameState/DefaultGameState.h"
+#include "FinalGameState.generated.h"
+
+UCLASS()
+class LISTENSERVERPROJECT_API AFinalGameState : public ADefaultGameState
+{
+	GENERATED_BODY()
+	
+};
